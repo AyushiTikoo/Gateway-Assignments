2adee407c8a4c6c5fddd328027a86488d65b7716
AyushiTikoo <53616087+AyushiTikoo@users.noreply.github.com>
2021-04-06 14:58:17 +0530

    Update Program.cs

--- TestingAssignment2/TestingAssignment2/Program.cs
+++ TestingAssignment2/TestingAssignment2/Program.cs
@@ -8,34 +8,34 @@ namespace TestingAssignment2
         {
             string inputStr = Console.ReadLine();
 
-            //Case 1  
+            // Case 1  
             Console.WriteLine("If Lowercase Characters Present Convert To Uppercase : " + inputStr.ConvertUpperCase());
 
-            //Case 2 
+            // Case 2 
             Console.WriteLine("If Uppercase Characters Present Convert To Lowercase : " + inputStr.ConvertLowerCase());
 
-            //Case 3  
+            // Case 3  
             Console.WriteLine("Title Case : " + inputStr.ConvertTitleCase());
 
-            //Case 4  
+            // Case 4  
             Console.WriteLine("To Check If Whole String Is In Lower Case Or Not : " + inputStr.CheckLowerCase());
 
-            //Case 5 
+            // Case 5 
             Console.WriteLine("First Letter Uppercase :" + inputStr.FirstUpperLetter());
 
-            //Case 6 
+            // Case 6 
             Console.WriteLine("To Check If Whole String Is In Upper Case Or Not : " + inputStr.CheckUpperCase());
 
-            //Case 7  
+            // Case 7  
             Console.WriteLine("Number Validation :" + inputStr.NumberValidation());
 
-            //Case 8 
+            // Case 8 
             Console.WriteLine("Last Character Remove : " + inputStr.LastCharacterRemove());
 
-            //Case 9 
+            // Case 9 
             Console.WriteLine("Word Count : " + inputStr.WordCount());
 
-            //Case 10  
+            // Case 10  
             Console.WriteLine("String To Int : " + inputStr.StringToInt());
         }
     }
